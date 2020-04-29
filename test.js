4877a572cbd835c9c2e1d5db15e3d72f92b9c540
MrAugu <39545629+MrAugu@users.noreply.github.com>
2020-04-29 18:07:26 +0300

    countEmojis, 5x faster than parseEmojis

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 const { EmojiParser, SpoilerParser } = require("./index");
 
-// console.log(
-//   EmojiParser.parseEmojis(
-// `<:redTick:663013890376073257> You cannot do that! ☕
-// <a:loading:393852367751086090> It's loading... ⏱️`
-//   )
-// );
+console.time("emoji-parser");
+console.log(
+  EmojiParser.countEmojis(
+`<:redTick:663013890376073257> You cannot do that! ☕
+<a:loading:393852367751086090> It's loading... ⏱️`
+  )
+);
+console.timeEnd("emoji-parser");
 
+console.time("spoiler-parser");
 console.log(SpoilerParser.parseSpoilers("in the movie, that other dude ||is killed|| and she ||married tom||"));
+console.timeEnd("spoiler-parser");
