4313c15252e7afeaabdc48c3b89ae45c4c4016f6
MrAugu <39545629+MrAugu@users.noreply.github.com>
2021-04-11 15:32:22 +0300

    Module Declaration Wrapper

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import { SpoilerParser as SpoilerParserClass } from "./parsers/Spoiler";
 import { Emoji as EmojiStructure } from "./structures/Emoji";
 import { Spoiler as SpoilerStructure } from "./structures/Spoiler";
 
-export const EmojiParser = EmojiParserClass;
-export const SpoilerParser = SpoilerParserClass;
-export const Emoji = EmojiStructure;
-export const Spoiler = SpoilerStructure;
+declare module "discord-message-parser" {
+  export const EmojiParser = EmojiParserClass;
+  export const SpoilerParser = SpoilerParserClass;
+  export const Emoji = EmojiStructure;
+  export const Spoiler = SpoilerStructure;
+}
